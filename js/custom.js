309f3db72b7aef7fee7b1bf7f936094f08b88157
John Moon <ymoon95@gmail.com>
2015-09-26 01:40:21 -0400

    Updated relevant Maps files

---
+++ js/custom.js
@@ -0,0 +1,123 @@
+// -------------------- Document on change functions --------------------
+
+
+
+// $('#pac-input').on('keypress', function(e) {
+//   // Only way to submit data is enter key
+//   if (e.which == 13) {
+//     updateLocation($('#pac-input'));
+//   }
+// });
+
+// function codeAddress(address) {
+  // geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       alert(results[0].geometry.location);
+//       return results[0].geometry.location;
+//     } else {
+//       alert("Geocode was not successful for the following reason: " + status);
+//     }
+//   });
+// }
+  
+
+// -------------------- Google Maps functions --------------------
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 20.9538989, lng: -21.1521341},
+    zoom: 3,
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length === 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+
+      // Firebase update
+      // updateLocation(place.geometry.location);
+
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+  // [END region_getplaces]
+}
+
+
+// -------------------- FIREBASE functions --------------------
+
+var ref = new Firebase("https://mapmyfeed.firebaseio.com");
+ref.authWithOAuthRedirect("twitter", function(error) {
+  if (error) {
+    console.log("Login Failed!", error);
+  }
+});
+
+
+var myFirebaseRef = new Firebase("https://mapmyfeed.firebaseio.com/");
+
+function updateLocation(coordinates) {
+  // alert(coordinates);
+  var locationsRef = ref.child("locations");
+  locationsRef.set({
+    city: {
+      location: coordinates,
+    }
+  });
+}
+
+
+
