62d94b3944f0a8ef5b127f14df5509a1bb2ffcc1
Katherine Pham <katpham@lawn-128-61-23-193.lawn.gatech.edu>
2015-09-26 11:11:39 -0400

    added js and buldings.html

---
+++ theFluNetwork.js
@@ -0,0 +1,23 @@
+
+// var req = new XMLHttpRequest();  
+// req.open('GET', 'http://http://m.gatech.edu/w/gtplaces/c/api/buildings', false);   
+// req.send(null);  
+// if(req.status == 200)  
+//    dump(req.responseText);
+
+// var request = require('request');
+// var cheerio = require('cheerio');
+
+// request('https://news.ycombinator.com', function (error, response, html) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(html);
+//   }
+// });
+
+
+
+// jQuery.get('http://localhost/MVC/gtbuildings.html', function(data) {
+// var myvar = data;
+// });
+
+window.alert('blah');
